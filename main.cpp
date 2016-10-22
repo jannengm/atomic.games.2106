49f4b3a2435f4b5ad6454e8d302f2233b47a06e6
Jason Lehmann <JasonMLehmann@gmail.com>
2016-10-22 13:09:56 -0400

    wooo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <string>
 //#include <c++/vector>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <iostream>
+#include <fstream>
 
 #define WIDTH 7
 #define HEIGHT 6
@@ -12,29 +21,31 @@ const string TEST_STR = "[[1,2,3,4,5,6,7],[8,9,0,1,2,3,4],[0,0,0,0,0,0,0],[0,0,0
 
 void read_json(const string & str, int board[HEIGHT][WIDTH]);
 void print_board(int board[HEIGHT][WIDTH]);
-
+int * get_Pmove(int board [HEIGHT][WIDTH]);
 int main(int argc, char ** argv) {
-    std::cout << TEST_STR << std::endl;
-
+    // std::cout << TEST_STR << std::endl;
+    
     int board[HEIGHT][WIDTH];
-    read_json(TEST_STR, board);
-    print_board(board);
+    // read_json(TEST_STR, board);
+    // print_board(board);
     if(argc > 1) {
-        cout << "Got from input: " << argv[1] << endl;
+        //  cout << "Got from input: " << argv[1] << endl;
+        read_json(argv[2], board);
+        print_board(board);
     }
     else{
         cout << "Invalid args" << endl;
     }
-
-//    return 1;
-    exit(1);
+    int a[] = get_Pmove(board);
+    //    return 1;
+    exit(a);
 }
 
 void read_json(const string & str, int board[HEIGHT][WIDTH]) {
     char ch;
     int size = HEIGHT * WIDTH;
     int iter = 0, row = 0, col = 0;
-
+    
     for (int i = 0; i < size;) {
         ch = str.at(iter++);
         if (ch >= '0' && ch <= '9') {
@@ -49,10 +60,25 @@ void read_json(const string & str, int board[HEIGHT][WIDTH]) {
 }
 
 void print_board(int board[HEIGHT][WIDTH]) {
+    cout << endl << endl;
     for(int row = 0; row < HEIGHT; row++){
         for(int col = 0; col < WIDTH; col++){
             cout << board[row][col] << " ";
         }
         cout << endl;
+    }
+    
+}
+
+
+int * get_Pmove( int board[HEIGHT][WIDTH])
+{
+    for(int row = 5; row < HEIGHT; row--){
+        for(int col = 0; col < WIDTH; col++){
+            if( board[row][col] == 0)
+                return col;
+            
+            
+        }
     }
 }
